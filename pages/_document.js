594824143b6132b6f2754eeb4222e7de03a40309
Jonathan Riboux <jonathan.riboux@gmail.com>
2022-06-12 13:56:39 +0200

    - Use victor font for prism contents

--- pages/_document.js
+++ pages/_document.js
@@ -35,6 +35,10 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap"
             rel="stylesheet"
           />
+          <link
+            rel="stylesheet"
+            href="https://cdn.jsdelivr.net/npm/victormono@latest/dist/index.min.css"
+          />
         </Head>
         <body className="bg-white text-black antialiased dark:bg-gray-900 dark:text-white">
           <Main />
