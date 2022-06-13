8dc8f5291475a0aedcaa4975d6a4734aa5c3cc95
Jonathan Riboux <jriboux@advisa.fr>
2022-06-13 14:31:08 +0200

    - Fix imageloader (use customLoader only in dev env)

--- components/Image.js
+++ components/Image.js
@@ -4,6 +4,9 @@ const customLoader = ({ src }) => {
   return src
 }
 
+const env = process.env.NODE_ENV
+const loaderProps = env === 'development' ? { loader: customLoader } : {}
+
 export default function Image(props) {
-  return <NextImage {...props} loader={customLoader} />
+  return <NextImage {...props} {...loaderProps} />
 }
