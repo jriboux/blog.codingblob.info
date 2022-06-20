3acecc41658c2ee4819477bc58029dcbd6d742b8
Jonathan Riboux <jriboux@advisa.fr>
2022-06-20 13:29:04 +0200

    - Switch to self hosted fonts from fontsource

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,8 @@ import '@/css/tailwind.css'
 import '@/css/prism.css'
 import 'katex/dist/katex.css'
 
-import '@fontsource/inter/variable-full.css'
+import '@fontsource/ubuntu'
+import '@fontsource/victor-mono'
 
 import { ThemeProvider } from 'next-themes'
 import Head from 'next/head'
--- pages/_document.js
+++ pages/_document.js
@@ -29,16 +29,6 @@ class MyDocument extends Document {
           <meta name="msapplication-config" content="/static/favicons/browserconfig.xml" />
           <meta name="theme-color" content="#333333" />
           <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap"
-            rel="stylesheet"
-          />
-          <link
-            rel="stylesheet"
-            href="https://cdn.jsdelivr.net/npm/victormono@latest/dist/index.min.css"
-          />
         </Head>
         <body className="bg-white text-black antialiased dark:bg-gray-900 dark:text-white">
           <Main />
